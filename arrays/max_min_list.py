2cfa8168e5d45bc0e7022d66d8c6914d4359ad21
Hmidreza <hmidreza@Hmidrezas-MacBook-Pro.local>
2024-02-12 14:31:41 +0330

    fix:change : to = in func

--- arrays/max_min_list.py
+++ arrays/max_min_list.py
@@ -9,12 +9,12 @@ max = 3 => array_ch = [1, 2, 3]
 """
 
 
-def check_input(array, min: None, max: None):
+def check_input(array, min=None, max=None):
 
-    check_min = lambda variable: True if min is None else variable >= min
-    check_max = lambda variable: True if max is None else variable <= max
+    check_min = lambda variable: True if min is None else (variable >= min)
+    check_max = lambda variable: True if max is None else (variable <= max)
 
     return [x for x in array if check_max(x) and check_min(x)]
 
 
-print(check_input([1, 2, 3, 4, 5, 6, 7, 8], min=4))
+print(check_input([1, 2, 3, 4, 5, 6, 7, 8], 4, 7))
