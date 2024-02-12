9166ffcdf85f808a0afd42c3c68a32419bdf99b1
Hmidreza <hmidreza@Hmidrezas-MacBook-Pro.local>
2024-02-12 11:30:43 +0330

    first commit

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM python-slim
+
+WORKDIR /app
+
+COPY . /app
+
+RUN pip install -r requirements.txt
+
+ENTRYPOINT [ "python" , "main.py" ]
---
+++ arrays/max_min_list.py
@@ -0,0 +1,10 @@
+"""
+in this we want
+do this : 
+ 
+array = [1, 2, 3, 4, 5, 6, 7, 8]
+min = 3 => array_ch = [3, 4, 5, 6, 7, 8]
+max = 3 => array_ch = [1, 2, 3]
+
+"""
+
