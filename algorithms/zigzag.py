1e6bf87f9ab299b70930b85f6a98e43c5bdaeec6
Hmidreza <hmidreza@Hmidrezas-MacBook-Pro.local>
2024-02-25 13:55:38 +0330

    feat: add class zigzag

--- algorithms/zigzag.py
+++ algorithms/zigzag.py
@@ -4,3 +4,20 @@ zigzag
 [1,3,5,7,9] , [2,4,6,8] => [1,2,3,4,5,6,7,8,9] 
 
 """
+
+
+class ZigZag:
+    def __init__(self, l1, l2):
+        self.quee = [l1, l2]
+
+def next(self):
+    v = self.quee.pop(0)
+    r = v.pop(0)
+    if v :
+        self.quee.append(v)
+    return r
+
+    def have_next(self):
+        if slef.quee:
+            return True
+        return False
