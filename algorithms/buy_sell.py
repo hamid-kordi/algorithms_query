b66350d272426a63d6038a0625f9423dac64fc24
Hmidreza <hmidreza@Hmidrezas-MacBook-Pro.local>
2024-02-25 11:23:07 +0330

    feat:add comment

---
+++ algorithms/buy_sell.py
@@ -0,0 +1,10 @@
+"""
+You buy at the first point and sell at the next point
+You need to find out which of them sells the most profit :
+
+[7, 1, 5, 3, 6, 4] => 5
+[9, 7, 6, 4, 3, 1] => 0
+"""
+
+
+
