402582d7f7414cfe79e887af0078a6a342ac09d6
Hmidreza <hmidreza@Hmidrezas-MacBook-Pro.local>
2024-02-25 16:15:25 +0330

    fix:add (for mdule)

--- algorithms/zigzag.py
+++ algorithms/zigzag.py
@@ -1,7 +1,7 @@
 """
 
 zigzag
-[1,3,5,7,9] , [2,4,6,8] => [1,2,3,4,5,6,7,8,9] 
+[1,3,5,7,9] , [2,4,6,8,10] => [1,2,3,4,5,6,7,8,9,10] 
 
 """
 
@@ -10,14 +10,19 @@ class ZigZag:
     def __init__(self, l1, l2):
         self.quee = [l1, l2]
 
-def next(self):
-    v = self.quee.pop(0)
-    r = v.pop(0)
-    if v :
-        self.quee.append(v)
-    return r
+    def next(self):
+        v = self.quee.pop(0)
+        r = v.pop(0)
+        if v:
+            self.quee.append(v)
+        return r
 
     def have_next(self):
-        if slef.quee:
+        if self.quee:
             return True
         return False
+
+
+z = ZigZag([1, 3, 5, 7, 9], [2, 4, 6, 8, 10])
+while z.have_next():
+    print(z.next())
