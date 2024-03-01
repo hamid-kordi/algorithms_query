25d05ec3490ee3c739147bcc9dd36c79e8fc969d
Hmidreza <hmidreza@Hmidrezas-MacBook-Pro.local>
2024-03-01 08:28:01 +0330

    feat:new algo for remov zero

---
+++ algorithms/remove_min.py
@@ -0,0 +1,16 @@
+"""
+move min :
+
+[4,5,6,8,2,7,1,-1,4,-6] => [4,5,6,8,2,7,1,-1,4]
+
+"""
+
+
+
+def move_min(lst):
+    temp = 0 
+    minimum = 0 
+    for n in lst:
+        if minimum > n :
+            minimum= n
+    return minimum
