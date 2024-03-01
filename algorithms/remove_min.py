4c0465c00e613325c1b72338a7abb68fa1946241
Hmidreza <hmidreza@Hmidrezas-MacBook-Pro.local>
2024-03-01 08:37:24 +0330

    fix:edit return first fun

--- algorithms/remove_min.py
+++ algorithms/remove_min.py
@@ -6,15 +6,26 @@ move min :
 """
 
 
-
 def move_min(lst):
-    temp = 0 
-    minimum = 0 
+    temp = []
+    minimum = 0
     for n in lst:
-        if minimum > n :
-            minimum= n
-    lst.remove(minimum)
+        if minimum > n:
+            minimum = n
+
+    for i in lst:
+        if i != minimum:
+            temp.append(i)
+    return temp
+
+
+### method
+
+
+def move_min_method(lst):
+
+    lst.remove(min(lst))
     return lst
 
 
-print(move_min([4,5,6,8,2,7,1,-1,4,-6]))
+print(move_min([4, 5, 6, 8, 2, 7, 1, -1, 4, -6]))
