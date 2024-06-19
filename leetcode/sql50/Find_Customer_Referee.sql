9b1654f7d2b3567cc3bfcce439b1647fb4c9ff90
Hmidreza <hmidreza@Hmidrezas-MacBook-Pro.local>
2024-06-19 11:50:31 +0330

    feat:add sql find customer

---
+++ leetcode/sql50/Find_Customer_Referee.sql
@@ -0,0 +1,36 @@
+-- Find the names of the customer that are not referred by the customer with id = 2.
+
+-- Return the result table in any order.
+
+-- The result format is in the following example.
+
+ 
+
+-- Example 1:
+
+-- Input: 
+-- Customer table:
+-- +----+------+------------+
+-- | id | name | referee_id |
+-- +----+------+------------+
+-- | 1  | Will | null       |
+-- | 2  | Jane | null       |
+-- | 3  | Alex | 2          |
+-- | 4  | Bill | null       |
+-- | 5  | Zack | 1          |
+-- | 6  | Mark | 2          |
+-- +----+------+------------+
+-- Output: 
+-- +------+
+-- | name |
+-- +------+
+-- | Will |
+-- | Jane |
+-- | Bill |
+-- | Zack |
+-- +------+
+
+-- Write your PostgreSQL query statement below
+SELECT name
+FROM Customer C
+WHERE C.referee_id IS DISTINCT FROM 2 ;
