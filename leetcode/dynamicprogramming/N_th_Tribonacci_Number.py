834e56c7f7104402354b8f1570945070e9bd119b
Hmidreza <hmidreza@Hmidrezas-MacBook-Pro.local>
2024-06-23 10:59:06 +0330

    fix:Improved code speed

--- leetcode/dynamicprogramming/N_th_Tribonacci_Number.py
+++ leetcode/dynamicprogramming/N_th_Tribonacci_Number.py
@@ -19,7 +19,8 @@ T_4 = 1 + 1 + 2 = 4
 """
 
 
-class Solution:
+# itt is not a good code in time spent
+class Solution_semi:
     def tribonacci(self, n: int) -> int:
         if n == 0:
             return 0
@@ -36,3 +37,40 @@ class Solution:
                 + Solution.tribonacci(self, n - 3)
             )
             return result
+
+
+# it is faster
+
+
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        result = [0, 1, 1, 2, 4]
+        if n > 4:
+            for i in range(5, n + 1):
+                temp = result[i - 1] + result[i - 2] + result[i - 3]
+                result.append(temp)
+        return result
+
+
+"""
+The following code is not for me,
+    but it is a very good example
+"""
+
+
+class Solution_exm:
+    def tribonacci(self, n: int) -> int:
+        if n < 3:
+            return 0 if n == 0 else 1
+
+        T = [0, 1, 1]
+
+        for index in range(3, n + 1):
+            T[0], T[1], T[2] = T[1], T[2], T[0] + T[1] + T[2]
+
+        return T[2]
+
+
+obj = Solution()
+
+print(obj.tribonacci(32))
