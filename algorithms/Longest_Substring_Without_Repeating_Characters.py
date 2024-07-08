895b6fa289aef482bd39d245a3ce6229c132e99a
Hmidreza <hmidreza@Hmidrezas-MacBook-Pro.local>
2024-07-08 16:30:33 +0330

    feat: add longest sub without repetaion character

---
+++ algorithms/Longest_Substring_Without_Repeating_Characters.py
@@ -0,0 +1,43 @@
+# Given a string s, find the length of the longest
+# substring
+#  without repeating characters.
+
+
+# Example 1:
+
+# Input: s = "abcabcbb"
+# Output: 3
+# Explanation: The answer is "abc", with the length of 3.
+# Example 2:
+
+# Input: s = "bbbbb"
+# Output: 1
+# Explanation: The answer is "b", with the length of 1.
+# Example 3:
+
+# Input: s = "pwwkew"
+# Output: 3
+# Explanation: The answer is "wke", with the length of 3.
+# Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        p1 = 0
+        seen = dict()
+        result = 0
+        for p2 in range(len(s)):
+            char = s[p2]
+            if char in seen and p2 >= 1:
+                p1 = seen[char] + 1
+            else:
+                result = max(result, p2 - p1 + 1)
+                print(result)
+            seen[char] = p2
+
+            print(seen)
+        return result
+
+
+obj = Solution()
+print(obj.lengthOfLongestSubstring("pwwkew"))
